e3cd2833964e5a14b43ef41c9a6e109b5b691963
ZhongLaiLu <1342341673@qq.com>
2021-02-01 15:40:07 +0800

    init map commpents

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,70 @@
+<!--
+ * @Descripttion: 
+ * @repository: https://github.com/luzhonglai
+ * @Author: ZhongLai Lu
+ * @Date: 2021-02-01 13:37:24
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2021-02-01 15:24:42
+-->
+/* eslint-disable no-undef */
+/* eslint-disable no-undef */
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-amap
+      :center="center"
+      :zoom="zoom"
+      :amap-manager="amapManager"
+      id="container"
+      :events="events"
+    >
+      <!-- 添加marker-->
+      <el-amap-marker
+        v-for="marker in list"
+        :key="marker.title"
+        :title="marker.title"
+        :position="marker.position"
+
+      ></el-amap-marker>
+    </el-amap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueAMap from 'vue-amap';
+const listData = require('./utils/data.json')
+
+let amapManager = new VueAMap.AMapManager();
 
 export default {
-  name: 'App',
+  name: 'map',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      center: [116.21213, 39.90923],
+      zoom: 8,
+      label: {
+        content: 'asdasds',
+        offset: [10, 12]
+      },
+      list: [],
+      amapManager,
+    }
+  },
+  created() {
+    this.initMap()
+  },
+  methods: {
+    initMap(){
+      let list = listData.map(item=> ({
+          title:item.name,
+          position : [item.location.lon, item.location.lat ],
+          isNew: item.isNew,
+          icon: item.isNew === 1 ? 'red' : 'yellow'
+      }))
+      this.list = list
+    }
+  },
 }
 </script>
 
@@ -23,6 +75,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#container {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
+/* eslint-disable no-undef */
+/*
+ * @Descripttion: 
+ * @repository: https://github.com/luzhonglai
+ * @Author: ZhongLai Lu
+ * @Date: 2021-02-01 13:37:24
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2021-02-01 14:15:44
+ */
 import Vue from 'vue'
 import App from './App.vue'
+// 引入vue-amap
+import VueAMap from 'vue-amap';
+Vue.use(VueAMap);
 
 Vue.config.productionTip = false
+
+VueAMap.initAMapApiLoader({
+  key: '申请的高德地图Key',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  uiVersion: '1.0.11' // 版本号
+});
 
 new Vue({
   render: h => h(App),
